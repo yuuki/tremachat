e0e229e631d0ccf99491b366e09f3d1ea91de7a1
y_uuki <yuki.tsubo@gmail.com>
2013-01-08 20:25:16 -0800

    Modify socket opt

--- lib/tremachat/client.rb
+++ lib/tremachat/client.rb
@@ -5,13 +5,13 @@ require 'tremachat/app/render'
 module Tremachat
   class Client
     include Socket::Constants
-    ETH_P_ALL    = 0x0300
+    TC_PROTOCOL = 134
 
     def initialize
       if RbConfig::CONFIG['host_os'] =~ /darwin/
-        @socket = Socket.open(AF_INET, SOCK_RAW, 0)
+        @socket = Socket.open(AF_INET, SOCK_RAW, TC_PROTOCOL)
       else
-        @socket = Socket.open(PF_PACKET, SOCK_DGRAM, ETH_P_IP)
+        @socket = Socket.open(AF_INET, SOCK_RAW, TC_PROTOCOL)
       end
       # off ip_header on userland
       @socket.setsockopt(IPPROTO_IP, IP_HDRINCL, 0)
@@ -19,11 +19,13 @@ module Tremachat
 
     def recv
       buff, ip_saddr = @socket.recvfrom(8041, MSG_WAITALL)
-      puts buff, ip_saddr
+      p buff
+      p ip_saddr
     end
 
     def send(message)
-      @socket.send(message, 0, '127.0.0.1')
+      sockaddr = Socket.sockaddr_in("discard", "localhost")
+      @socket.send(message, 0, sockaddr)
     end
   end
 end
